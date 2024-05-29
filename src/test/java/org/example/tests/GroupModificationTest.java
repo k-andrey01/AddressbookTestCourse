25c9edf682a936cef0367c13f3e98e0791d52bdb
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2024-05-29 15:38:24 +0300

    ВЫнесены некоторые дублирующиеся части кода в тестах модификации и удаления групп

--- src/test/java/org/example/tests/GroupModificationTest.java
+++ src/test/java/org/example/tests/GroupModificationTest.java
@@ -1,36 +1,34 @@
 package org.example.tests;
 
 import org.example.model.GroupData;
+import org.junit.Before;
 import org.junit.Test;
 import org.testng.Assert;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase{
 
-    @Test
-    public void testGroupModification(){
+    @Before
+    public void ensurePreconditions(){
         app.getNavigationHelper().goToGroupPage();
-
         if (!app.getGroupHelper().isWhereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+
+    @Test
+    public void testGroupModification(){
         List<GroupData> before = app.getGroupHelper().getGroupList();
+        int index = before.size() -1;
+        GroupData group = new GroupData(before.get(index).getId(), "group3", "mod", "modr");
+        app.getGroupHelper().modifyGroup(index, group);
 
-        app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().initGroupModification();
-
-        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "group3", "mod", "modr");
-        app.getGroupHelper().fillGroupForm(group);
-        app.getGroupHelper().submitGroupModification();
-
-        app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(group);
         Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
         before.sort(byId);
