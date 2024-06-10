eea4ec0319ffb5ccc2ea8111c29a6785d474df92
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2024-06-10 11:59:37 +0300

    Реализовано использование данных из файла для теста создания группы

--- src/test/java/org/example/tests/CreateGroupTest.java
+++ src/test/java/org/example/tests/CreateGroupTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -24,11 +24,15 @@ public class CreateGroupTest extends TestBase {
   }
 
   @Parameterized.Parameters
-  public static Collection<Object[]> validGroups() {
+  public static Collection<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<>();
-    list.add(new Object[] {new GroupData().withGroupName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[] {new GroupData().withGroupName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[] {new GroupData().withGroupName("test3").withHeader("header 3").withFooter("footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withGroupName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list;
   }
 
