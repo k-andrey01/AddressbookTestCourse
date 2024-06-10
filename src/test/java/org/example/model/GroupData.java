fd3b1a167fb56d023e3bff6f4626079ec2009b13
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2024-06-10 15:52:41 +0300

    Реализована работа с json-файлами

--- src/test/java/org/example/model/GroupData.java
+++ src/test/java/org/example/model/GroupData.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -9,9 +10,12 @@ import java.util.Objects;
 public class GroupData {
 
     @XStreamOmitField
-    private int id = Integer.MAX_VALUE;;
+    private int id = Integer.MAX_VALUE;
+    @Expose
     private String groupName;
+    @Expose
     private String header;
+    @Expose
     private String footer;
 
     public String getGroupName() {
